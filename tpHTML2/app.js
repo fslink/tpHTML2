deb96dc21975cffacbe7a48123ae139f9397734e
unknown <mowglysofiann@hotmail.com>
2018-08-31 17:05:09 +0200

    second commit

--- tpHTML2/app.js
+++ tpHTML2/app.js
@@ -14,6 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+/*app.use('/js', express.static(path.join(__dirname, 'public',
+ 'javascript')));
+app.use('/css', express.static(path.join(__dirname, 'public',
+ 'stylesheets')));*/
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
